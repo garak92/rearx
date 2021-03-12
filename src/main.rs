cf5c862e92f93287b599584ff8d81914eb6ab95d
Elim Garak <garak92@protonmail.com>
2021-03-12 14:08:12 -0300

    Termion added for keybinding purposes; for the moment, able to open first result by pressing the backspace key

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use serde::{Deserialize, Serialize};
 #[macro_use]
 extern crate prettytable;
 use prettytable::{Cell, Row, Table};
+use serde::{Deserialize, Serialize};
+use std::io::{stdin, stdout, Write};
+use std::process::Command;
+use termion::cursor;
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 
 fn main() {
     match deserialize_json() {
@@ -16,7 +22,7 @@ fn main() {
 
 #[tokio::main]
 async fn deserialize_json() -> Result<RequestData, Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://searx.garudalinux.org/search?q=solar%20panels&categories=general&format=json&lang=en&page=1")
+    let resp = reqwest::get("https://searx.garudalinux.org/search?q=karl%20poppers&categories=general&format=json&lang=en&page=1")
         .await?
         .json::<RequestData>()
         .await?;
@@ -43,14 +49,48 @@ fn create_table(data: RequestData) {
     let contents: Vec<Content> = data.results;
     let lenght = contents.len();
     let mut table = Table::new();
-    table.set_titles(row![b->"Title", b->"Search Engine", b->"Content", b->"URL"]);
+    table.set_titles(row![b->"No", b->"Title", b->"Search Engine", b->"URL"]);
+
     for i in 0..lenght {
         table.add_row(row![
+            i,
             contents[i].title,
             contents[i].engine,
-            contents[i].content,
             contents[i].pretty_url
         ]);
     }
     table.printstd();
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    for c in stdin.keys() {
+        write!(
+            stdout,
+            "{}{}",
+            termion::cursor::Goto(1, 1),
+            termion::clear::CurrentLine
+        )
+        .unwrap();
+
+        match c.unwrap() {
+            Key::Char('q') => break,
+            Key::Char(c) => println!("{}", c),
+            Key::Alt(c) => println!("^{}", c),
+            Key::Ctrl(c) => println!("*{}", c),
+            Key::Esc => println!("ESC"),
+            Key::Left => println!("←"),
+            Key::Right => println!("→"),
+            Key::Up => println!("↑"),
+            Key::Down => println!("↓"),
+            Key::Backspace => {
+                Command::new("sh")
+                    .arg("xdg-open")
+                    .arg(&contents[0].pretty_url)
+                    .output()
+                    .expect("failed to execute process");
+            }
+            _ => {}
+        }
+        stdout.flush().unwrap();
+    }
 }
